86d764225a3c8306276a014737d275b6b9a01886
Pineapple Bun <35952622+asdfg44l@users.noreply.github.com>
2022-03-18 17:14:08 +0800

    Code cleaning: 清除不必要的 cdn 與 require

--- src/entry.js
+++ src/entry.js
@@ -1,12 +1,5 @@
-if(process.env.NODE_ENV === 'development') {
-    require('./pages/index.hbs')
-}
-
 //require javascript
-require('./javascript/test.js')
 require('./javascript/index.js')
-
 
 //import scss
 import './assets/scss/all.scss'
-
--- src/javascript/test.js
+++
@@ -1,5 +0,0 @@
-// const Handlebars = require('handlebars')
-
-// Handlebars.registerHelper('toJSON', function(obj) {
-//     return JSON.stringify(obj);
-// });
